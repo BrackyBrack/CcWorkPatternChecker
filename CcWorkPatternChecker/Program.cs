74fb93bfcf54210431c47d3cf4672e8fbe40b547
David <david_bracken@btinternet.com>
2020-06-10 12:39:32 +0100

    Add project files.

---
+++ CcWorkPatternChecker/Program.cs
@@ -0,0 +1,75 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CcWorkPatternChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable crewPanels = CsvReader.GetCrewPanels();
+            DataTable jsPanels = CsvReader.GetPanels();
+            DataTable plannedJs = CrewData.GetOffPeriods();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (DataRow crewRow in crewPanels.Rows)
+            {
+                string partTimeType = crewRow["Part Time Type"].ToString();
+
+                if(partTimeType != "FT")
+                {
+                    string crewMember = crewRow[0].ToString();
+
+                    var crewPlannedJs = plannedJs.Select($"P_LTR_CODE = '{crewMember}'");
+
+                    List<DateTime> plannedDates = GetPlannedDates(crewPlannedJs);
+
+                    foreach (DataRow jsRow in jsPanels.Rows)
+                    {
+                        if (jsRow[partTimeType].ToString() == "1")
+                        {
+                            DateTime panelDate = DateTime.Parse(jsRow[0].ToString());
+                            if (plannedDates.Contains(panelDate) == false)
+                            {
+                                sb.AppendLine($"{crewMember} does not match panel {partTimeType}");
+                                break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    string crewMember = crewRow[0].ToString();
+
+                    var crewPlannedJs = plannedJs.Select($"P_LTR_CODE = '{crewMember}'");
+
+                    if(crewPlannedJs.Count() > 0)
+                    {
+                        sb.AppendLine($"{crewMember} does not match panel {partTimeType}");
+                        break;
+                    }
+                }
+            }
+
+            System.IO.File.WriteAllText("C:\\Users\\david.bracken\\OneDrive - TUI\\Documents\\Furlough\\CC Infor\\Panel Errors.txt", sb.ToString());
+        }
+
+        private static List<DateTime> GetPlannedDates(DataRow[] crewPlannedJs)
+        {
+            List<DateTime> result = new List<DateTime>();
+
+            foreach (DataRow row in crewPlannedJs)
+            {
+                DateTime date = DateTime.Parse(row[1].ToString());
+                result.Add(date);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DataAccess/CsvReader.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Runtime.InteropServices;
+
+namespace DataAccess
+{
+    public static class CsvReader
+    {
+
+        public static DataTable GetPanels()
+        {
+            DataSet ds = new DataSet();
+            ds.Tables.Add(ExcelToDataTable("C:\\Users\\david.bracken\\OneDrive - TUI\\Documents\\Furlough\\CC Infor\\New Panels.xlsx"));
+            return ds.Tables[0];
+        }
+
+        public static DataTable GetCrewPanels()
+        {
+            return ExcelToDataTable("C:\\Users\\david.bracken\\OneDrive - TUI\\Documents\\Furlough\\CC Infor\\NewPatterns.xlsx");
+        }
+
+        private static DataTable ExcelToDataTable(string excelFilePath)
+        {
+            Microsoft.Office.Interop.Excel.Application application = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook workbook = application.Workbooks.Open(excelFilePath, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            Microsoft.Office.Interop.Excel.Worksheet worksheet = workbook.Worksheets[1];
+
+            DataTable dt = WorksheetToDataTable(worksheet);
+            workbook.Close();
+            Marshal.ReleaseComObject(worksheet);
+            Marshal.ReleaseComObject(workbook);
+            Marshal.ReleaseComObject(application);
+            return dt;
+        }
+
+        private static DataTable WorksheetToDataTable(Microsoft.Office.Interop.Excel.Worksheet worksheet)
+        {
+            Microsoft.Office.Interop.Excel.Range range = worksheet.UsedRange;
+            object[,] valueArray = (object[,])range.Value;
+
+            DataTable dt = new DataTable();
+
+            //Add columns and set column names to values in row 1 of worksheet
+            for (int i = 1; i < valueArray.GetUpperBound(1) + 1; i++)
+            {
+                string columnName = valueArray[1, i]?.ToString();
+
+                if (string.IsNullOrWhiteSpace(columnName))
+                {
+                    columnName = "Column " + i.ToString();
+                }
+
+                dt.Columns.Add(columnName);
+            }
+
+            //Add rows to datatable starting at row 2 of worksheet
+            for (int i = 2; i < valueArray.GetUpperBound(0) + 1; i++)
+            {
+                DataRow row = dt.NewRow();
+                int cell = 0;
+                for (int j = 1; j < dt.Columns.Count + 1; j++)
+                {
+                    row[cell] = valueArray[i, j];
+                    cell++;
+                }
+                dt.Rows.Add(row);
+            }
+
+            return dt;
+        }
+    }
+}
